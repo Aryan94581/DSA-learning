0fd9131b9ab5fab1dc7e04292cfe93b5f975ee66
Aryan94581 <swamiaryan611@gmail.com>
2024-06-04 19:24:00 +0530

    commit

--- Main.java
+++ Main.java
@@ -1,17 +1,26 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: doubble linkedlist
-            DLL list = new DLL();
-        list. insertFirst(13);
-        list. insertFirst(12);
-        list. insertFirst(18);
-        list. insertFirst(16);
+        DLL list = new DLL();
+        list.insertFirst(13);
+        list.insertFirst(15);
+        list.insertFirst(17);
+        list.insertFirst(19);
+        list.insertFirst(11);
         list.insertLast(99);
+        list.insert(17, 88);
+        System.out.println(list.find(17));
+
         list.display();
-        System.out.println();
-        list.insert(12, 88);
-        list.display();
-        // for single linkedList 
+
+        // list. insertFirst(12);
+        // list. insertFirst(18);
+        // list. insertFirst(16);
+        // list.insertLast(99);
+        // list.display();
+        // System.out.println();
+
+        // TODO: for single linkedList
         // LL list = new LL();
         // list.insertFirst(15);
         // list.insertFirst(13);
@@ -19,8 +28,8 @@ public class Main {
         // list.insertFirst(19);
         // list.insertLast(16);
         // list.insert(77, 3);
-        // // list.insert(66, 100); give exception 
-       
+        // // list.insert(66, 100); give exception
+
         // list.display();
         // System.err.println(list.find(13));
         // System.err.println(list.find(77));
