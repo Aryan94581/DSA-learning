68878f890ccaf80fe34b92c27f24effe1267e5a4
Aryan94581 <swamiaryan611@gmail.com>
2024-06-08 20:46:32 +0530

    update

---
+++ Hashing.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+public class Hashing {
+    public static void main(String[] args) {
+        //TO declare a Hashsat
+        HashSet<Integer> set = new HashSet<>();
+        set.add(4);
+        set.add(41);
+        set.add(42);
+        set.add(43);
+        set.add(44);
+        set.add(48);
+        
+        
+        
+    }
+}
