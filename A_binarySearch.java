64c0b44d1b59fb45c322fc38dceeab69238aff99
Aryan94581 <swamiaryan611@gmail.com>
2024-06-15 17:12:06 +0530

    update

--- A_binarySearch.java
+++ A_binarySearch.java
@@ -1,4 +1,27 @@
 public class A_binarySearch {
+public static void main(String[] args) {
+    
+}
+static int binarySearch(int[] arr, int target){
+    if(arr.length <= 0 ){
+        return -1;
+    }
 
+    int start = 0 ,end = arr.length-1;
+    
+    while(start <= end){
+        int mid = mid(start, end);
+        if(target < arr[mid]){
+            end = mid-1;
+        }else{
+            
+        }
+        return mid;
+    }
+    return -1;
+}
+    static int mid(int start, int end){
+        return start+end/2;
+    }
     
 }
