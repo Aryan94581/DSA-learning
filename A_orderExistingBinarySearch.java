189b0f3de9b2d862b3d68e6a1b338b3f5c047f2a
Aryan94581 <swamiaryan611@gmail.com>
2024-06-17 16:15:28 +0530

    UPDATE

--- A_orderExistingBinarySearch.java
+++ A_orderExistingBinarySearch.java
@@ -1,7 +1,9 @@
 public class A_orderExistingBinarySearch {
     public static void main(String[] args) {
         int[] array = {1,3,6,8,15,24,38,49,52,58,69,83};
+        int[] decArray = {88,87,86,83,75,53,47,32,21,12,11};
         System.out.println("your value is at index of :" + binarySearch(array, 83));
+        System.out.println("your value is at index of :" + binarySearch(decArray, 87));
         
     }
     
@@ -10,18 +12,18 @@ public class A_orderExistingBinarySearch {
             return -1;
         }
         
-        if(arr[0]> arr[arr.length-1]){
+        if(arr[0] > arr[arr.length-1]){
 
         int start = 0 ,end = arr.length;
             
-            while(start >= end){
+            while(start <= end){
                 int mid = mid(start, end);
                 if (target == arr[mid]) {
                     return mid;
-                }else if(target > arr[mid]){
-                    end = mid-1;
-                }else{
+                }else if(target < arr[mid]){
                     start = mid+1;
+                    }else{
+                    end = mid-1;
                     }
             }
             return -1;
