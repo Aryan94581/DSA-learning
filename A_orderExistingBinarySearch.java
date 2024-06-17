9a67caf7b932480ebcb3ea99098f5daa0d03f88f
Aryan94581 <swamiaryan611@gmail.com>
2024-06-17 16:08:00 +0530

    update

--- A_orderExistingBinarySearch.java
+++ A_orderExistingBinarySearch.java
@@ -6,43 +6,40 @@ public class A_orderExistingBinarySearch {
     }
     
     static int binarySearch(int[] arr, int target){
-
-        if(arr[0]> arr[arr.length-1]){
-            if(arr.length <= 0 ){
-                return -1;
-            }
+        if(arr.length <= 0 ){
+            return -1;
+        }
         
-            int start = 0 ,end = arr.length;
+        if(arr[0]> arr[arr.length-1]){
+
+        int start = 0 ,end = arr.length;
             
-            while(start <= end){
+            while(start >= end){
                 int mid = mid(start, end);
                 if (target == arr[mid]) {
                     return mid;
-                }else if(target < arr[mid]){
+                }else if(target > arr[mid]){
                     end = mid-1;
                 }else{
                     start = mid+1;
-                }
+                    }
             }
             return -1;
         }else{
-            if(arr.length <= 0 ){
-                return -1;
-            }
+
+        int start = 0 ,end = arr.length;
         
-            int start = 0 ,end = arr.length;
-            
-            while(start <= end){
-                int mid = mid(start, end);
-                if (target == arr[mid]) {
-                    return mid;
-                }else if(target < arr[mid]){
-                    end = mid-1;
-                }else{
-                    start = mid+1;
-                }
+        while(start <= end){
+            int mid = mid(start, end);
+            if (target == arr[mid]) {
+                return mid;
+            }else if(target < arr[mid]){
+                end = mid-1;
+            }else{
+                start = mid+1;
             }
-            return -1;
+        }
+        return -1;
         }
     }
 
