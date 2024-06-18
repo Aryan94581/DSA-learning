9842d65508bd3c21e89d77f57e288f2717e7c2f6
Aryan94581 <swamiaryan611@gmail.com>
2024-06-18 11:00:58 +0530

    update

--- A_ceilingBs.java
+++ A_ceilingBs.java
@@ -51,3 +51,19 @@ static int floorbinarySearch(int[] arr, int target){
         return (start+end)/2;
     }
 }
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        
+        int start = 0;
+        int end = letters.length-1;
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            if(target > letters[mid]){
+                start = mid+1;
+            }else {
+                end = mid-1;
+            }
+        }
+        return letters[start % (letters.length)];
+    }
+}
