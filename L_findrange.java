c225319831d6fb8f2d3cb86128fa76d579b30c35
Aryan94581 <swamiaryan611@gmail.com>
2024-06-18 17:29:19 +0530

    update

--- L_findrange.java
+++ L_findrange.java
@@ -8,6 +8,13 @@ class L_findrange {
 
 
     public  static int[] searchRange(int[] nums, int target) {
+        int[] ans = {-1, -1};
+        
+        
+    return ans;
+    }
+    static int biSearch(int[] nums, int target, boolean findStartIndex){
+        int ans = -1;
         int start = 0;
         int end = nums.length-1;
 
@@ -15,12 +22,19 @@ class L_findrange {
             int mid = (start + end)/2;
             if(target < nums[mid]){
                 end = mid-1;
-            }else{
+            }else if(target > nums[mid]){
                 start = mid+1;
+            }else{
+            // potential mid
+            ans = mid;
+            if(findStartIndex == true){
+                end = mid-1;
+            } else{
+                start = mid+1; 
+            }
             }
         }
-        return mid;
-        
+        return ans;
     }
 }
 
