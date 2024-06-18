1d9ad418d56f2003887030c6225f80b0c6d2cff0
Aryan94581 <swamiaryan611@gmail.com>
2024-06-18 09:11:15 +0530

    update

--- A_ceilingBs.java
+++ A_ceilingBs.java
@@ -3,6 +3,8 @@ public class A_ceilingBs {
 public static void main(String[] args) {
     int[] array = {1,3,6,8,15,24,38,49,52,58,69,83};
     System.out.println("the Cling number is : " + clingbinarySearch(array, 56));
+    System.out.println("the floor number is : " + floorbinarySearch(array, 56));
+
     
 }
 static int clingbinarySearch(int[] arr, int target){
@@ -23,6 +25,25 @@ static int clingbinarySearch(int[] arr, int target){
         }
     }
     return arr[start];
+}
+static int floorbinarySearch(int[] arr, int target){
+    if(arr.length <= 0 ){
+        return -1;
+    }
+
+    int start = 0 ,end = arr.length;
+    
+    while(start <= end){
+        int mid = mid(start, end);
+        if (target == arr[mid]) {
+            return mid;
+        }else if(target < arr[mid]){
+            end = mid-1;
+        }else{
+            start = mid+1;
+        }
+    }
+    return arr[end];
 }
     static int mid(int start, int end){
         return (start+end)/2;
