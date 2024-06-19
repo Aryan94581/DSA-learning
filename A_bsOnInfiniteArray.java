549aa94f15b484940abf534dd85bb20c89b3d838
Aryan94581 <swamiaryan611@gmail.com>
2024-06-19 19:40:46 +0530

    update

--- A_bsOnInfiniteArray.java
+++ A_bsOnInfiniteArray.java
@@ -1,7 +1,7 @@
 public class A_bsOnInfiniteArray {
     public static void main(String[] args) {
-        int[] array ={2, 3, 4, 6, 8, 15, 24, 38, 49, 52, 58, 69, 83};
-        int target = 38;
+        int[] array ={2,3,4,6,8,15,24,38,49,52,58,69,83};
+        int target = 49;
         System.out.println(findingAns(array, target));
 
     }
@@ -10,9 +10,8 @@ public class A_bsOnInfiniteArray {
         // start with the box of size 2
         int start = 0;
         int end = 1;
-        
         // condition of the target lie in the range 
-        while (target <= arr[end]){
+        while (target >= arr[end]){
             int newstart = end+1;
             end = end + (end - start + 1)*2;
             start = newstart;
@@ -21,11 +20,11 @@ public class A_bsOnInfiniteArray {
     }
     static int binarySearch(int[] arr, int target, int start, int end){
         while(start <= end){
-            int mid = (start+end)/2;
+            int mid = (start + end)/2;
             if(target > arr[mid]){
-                start = mid+1;
+                start = mid + 1;
             }else if(target < arr[mid]){
-                end = mid-1;
+                end = mid - 1;
             }else{
                 return mid;
             }
