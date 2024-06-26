524f814a671d44042fb211c795abb6e0fb6ed8d9
Aryan94581 <swamiaryan611@gmail.com>
2024-06-26 22:17:16 +0530

    update

--- A_search2dArrayOr.java
+++ A_search2dArrayOr.java
@@ -2,10 +2,20 @@ public class A_search2dArrayOr {
 public static void main(String[] args) {
     
 }
-static int ArrSearch(int[][] matrix, int target){
+static int[] ArrSearch(int[][] matrix, int target){
     int row = 0;
     int col = matrix.length-1;
-    
+    while(row < matrix.length-1 && col >= 0){
+        if(matrix[row][col] == target){
+            return new int[]{row, col};
+        }
+        if(matrix[row][col] < target){
+            row++;
+        }else{
+            col--;
+        }
+
+    }
 
     return -1;
 }
