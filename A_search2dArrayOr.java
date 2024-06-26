b177a97fe232e05adf5a4db69ac2d9e8366d76be
Aryan94581 <swamiaryan611@gmail.com>
2024-06-26 19:59:42 +0530

    update

--- A_search2dArrayOr.java
+++ A_search2dArrayOr.java
@@ -1,3 +1,13 @@
 public class A_search2dArrayOr {
+public static void main(String[] args) {
+    
+}
+static int ArrSearch(int[][] matrix, int target){
+    int row = 0;
+    int col = matrix.length-1;
+    
+
+    return -1;
+}
     
 }
