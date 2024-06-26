9e0d0c050cb28bbb98784ce8f00c217bc2b5fd2e
Aryan94581 <swamiaryan611@gmail.com>
2024-06-26 23:24:57 +0530

    update

--- A_bubbleSort.java
+++ A_bubbleSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 public class A_bubbleSort {
     public static void main(String[] args) {
     int[] arr = {23, 12, 22, 11, 3};    
-    System.out.println(Arrays.toString(arr));
+    System.out.println(Arrays.toString(bubbleSort(arr)));
     }
     static int[] bubbleSort(int[] arr){
         int length = arr.length-1;
@@ -12,13 +12,14 @@ public class A_bubbleSort {
 
                 for (int i = 1; i < length+1; i++) {
                     
-                    if(arr[i] > arr[i-1]){
-                    int temp = arr[i];
-                    arr[i] = arr[i-1];
-                    arr[i-1] = temp;
+                    if(arr[i] < arr[i-1]){
+                        int temp = arr[i];
+                        arr[i] = arr[i-1];
+                        arr[i-1] = temp;
+                    }
                 }
+                length--;
             }
-
         return arr;
     }
 
