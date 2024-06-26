e8494d40fd69f7ed3fbaba52a3da75467e9e821b
Aryan94581 <swamiaryan611@gmail.com>
2024-06-26 22:21:00 +0530

    update

--- A_search2dArrayOr.java
+++ A_search2dArrayOr.java
@@ -1,5 +1,13 @@
 public class A_search2dArrayOr {
 public static void main(String[] args) {
+    int[][] arr = {
+        {2, 5, 8, 13},
+        {3, 7, 9, 15},
+        {4, 10, 17, 23},
+        {6, 12, 22, 27}
+    };
+    int target = 22;
+    System.out.println(ArrSearch(arr, target)); 
     
 }
 static int[] ArrSearch(int[][] matrix, int target){
@@ -17,7 +25,7 @@ static int[] ArrSearch(int[][] matrix, int target){
 
     }
 
-    return -1;
+    return new int[]{-1, -1};
 }
     
 }
