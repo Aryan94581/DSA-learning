143cb812cad68f54270ed81dbefaefb201b302ad
Aryan94581 <swamiaryan611@gmail.com>
2024-06-29 23:52:24 +0530

    update

--- A_insertionSort.java
+++ A_insertionSort.java
@@ -9,9 +9,25 @@ public class A_insertionSort {
         System.out.println(Arrays.toString(array));
     }
 
-    public static void insertionSort(int[] array) {
+    public static void insertionSort(int[] arr) {
+        for (int i = 0; i < arr.length-2; i++) {
+            int j = i+1;
+            while(j > 0){
+            if(arr[j] < arr[i] ){
+                swap(arr, i, j);
+                j--;
+            }else{
+                break;
+            }
+        }
+    }
         
 
+    }
+    static void swap(int[] arr, int index1, int index2){
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
     }
 
 }
