45ba9a1d1640d32fe264e648da43d4e9a3e7feca
Aryan94581 <swamiaryan611@gmail.com>
2024-06-30 23:06:44 +0530

    update

--- A_CyclicSort.java
+++ A_CyclicSort.java
@@ -1,13 +1,19 @@
+
+import java.util.Arrays;
+
 public class A_CyclicSort {
     public static void main(String[] args) {
-        
+        int[] array = {3, 8, 5, 9, 2, 6, 1, 4, 7};
+        cyclicSort(array);
+        System.out.println(Arrays.toString(array));
         
     }
     static void cyclicSort(int[] arr){
-
-        for (int i = 0; i < arr.length-1;) {
-            if(i != arr[i]){
-                swap(arr, (arr[i]-1), i);
+        int i = 0;
+        while (i < arr.length) {
+            int correctInd = arr[i]-1;
+            if(i != correctInd){
+                swap(arr, correctInd, i);
             }else{
                 i++;
             }
