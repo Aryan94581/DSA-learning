ab1426ed668cca9c53ac2f4c1c82907ab3e3fea2
Aryan94581 <swamiaryan611@gmail.com>
2024-07-01 19:20:30 +0530

    update

---
+++ L_missingNumber.java
@@ -0,0 +1,12 @@
+public class L_missingNumber {
+    // 268. Missing Number
+    public int missingNumber(int[] nums) {
+        int sumOfArray = 0;
+        int sumOfIndex = 0;
+        for(int i = 0; i< nums.length; i++){
+            sumOfArray += nums[i];
+            sumOfIndex += i+1;
+        }
+        return sumOfIndex - sumOfArray; 
+    }
+}
