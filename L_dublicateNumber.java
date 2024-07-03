de9747044b0a8f4540e7928579217261b7e3a61f
Aryan94581 <swamiaryan611@gmail.com>
2024-07-03 23:20:47 +0530

    update

--- L_dublicateNumber.java
+++ L_dublicateNumber.java
@@ -1,24 +1,41 @@
-// 287. Find the Duplicate Number
-public class L_dublicateNumber {
-    public int findDuplicate(int[] nums) {
+import java.util.ArrayList;
+import java.util.List;
+
+class L_dublicateNumber {
+
+public static void main(String[] args) {
+        int[] nums = {5, 4, 6, 7, 9, 3, 10, 9, 5, 6};
+        List<Integer> arr = findDuplicates(nums);
+        System.out.println(arr);
+            
+        }
+
+    public static List<Integer> findDuplicates(int[] nums) {
+        
+        List<Integer> list = new ArrayList<>();
         int i = 0;
-        while (i < nums.length){
-                int correctIndex = nums[i]-1;
-                    if(i != correctIndex){
-                        if(nums[i] == nums[correctIndex]){                        
-                            return nums[i];
-                        }else{
-                        swap(nums, i, correctIndex);
-                        }
-                    }else{
-                        i++;
+        while(i < nums.length) {
+            int correctIndex = nums[i] - 1;
+            if(i != correctIndex) {
+                if(nums[i] == nums[correctIndex]) {
+                    if(!list.contains(nums[i])) {
+                        list.add(nums[i]);
                     }
+                } else {
+                    swap(nums, i, correctIndex);
                 }
-        return -1;
+            } else {
+                i++;
+            }
+        }
+        return list;
     }
-    public static void swap(int[] arr, int indexFir, int indexSec){
-        int temp = arr[indexFir];
-        arr[indexFir] = arr[indexSec];
-        arr[indexSec] = temp;
+
+    static void swap(int[] arr, int index1, int index2) {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
     }
+
+   
 }
