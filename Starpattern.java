84a8ef2bb7aa576c4b791c4c53c714534bb92000
Aryan94581 <swamiaryan611@gmail.com>
2024-07-07 23:00:12 +0530

    update

--- Starpattern.java
+++ Starpattern.java
@@ -11,8 +11,9 @@ public class Starpattern {
         }
         System.out.println("");
         System.out.println("2. Type");
+        System.out.println("");
         for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j < i; j++) {
+            for (int j = 0; j < i; j++) {
                 System.out.print("*");
             }
             System.out.println("");    
@@ -51,8 +52,49 @@ public class Starpattern {
             System.out.println("");    
         }
         System.out.println("");
-        System.out.println("5. Type ");
+        System.out.println("6. Type ");
         System.out.println("");
-        
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 5; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+        System.out.println("7. Type ");
+        System.out.println("");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 5; k > i; k--) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+        System.out.println("8. Type ");
+        System.out.println("");
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 5; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        for (int i = 1; i < 5; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 5; k > i; k--) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
     }
 }
