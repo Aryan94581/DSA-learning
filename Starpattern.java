bb98c73a4ec67d73f844045373789d50f9881ed4
Aryan94581 <swamiaryan611@gmail.com>
2024-07-07 21:54:08 +0530

    update

--- Starpattern.java
+++ Starpattern.java
@@ -1,13 +1,32 @@
 public class Starpattern {
     
     public static void main(String[] args) {
-        for (int i = 0; i <= 5; i++) {
+        System.out.println("1 .Type");
+        System.out.println("");
+        for (int i = 1; i < 5; i++) {
+            for (int j = 1; j < 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+        System.out.println("2. Type");
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");    
+        }
+        System.out.println("");
+        System.out.println("3. Type ");
+        for (int i = 5; i >0; i--) {
             for (int j = 0; j < i; j++) {
                 System.out.print("*");
             }
             System.out.println("");    
         }
         System.out.println("");
+        System.out.println("4. Type ");
         
     }
 }
