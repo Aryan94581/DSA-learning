5edc29b5aa1e59014c73c358cfe0bfeddc6afcc1
Aryan94581 <swamiaryan611@gmail.com>
2024-07-08 09:53:22 +0530

    update

--- Starpattern.java
+++ Starpattern.java
@@ -5,7 +5,7 @@ public class Starpattern {
         System.out.println("");
         for (int i = 1; i < 5; i++) {
             for (int j = 1; j < 5; j++) {
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println("");
         }
@@ -14,7 +14,7 @@ public class Starpattern {
         System.out.println("");
         for (int i = 1; i <= 5; i++) {
             for (int j = 0; j < i; j++) {
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println("");    
         }
@@ -24,7 +24,7 @@ public class Starpattern {
 
         for (int i = 5; i >0; i--) {
             for (int j = 0; j < i; j++) {
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println("");    
         }
@@ -32,13 +32,13 @@ public class Starpattern {
         System.out.println("4. Type ");
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j < i; j++) {
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println("");    
         }
         for (int i = 5; i >0; i--) {
             for (int j = 0; j < i; j++) {
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println("");    
         }
@@ -47,7 +47,7 @@ public class Starpattern {
         System.out.println("");
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <=i; j++) {
-                System.out.print(j);
+                System.out.print(j+" ");
             }
             System.out.println("");    
         }
@@ -56,10 +56,10 @@ public class Starpattern {
         System.out.println("");
         for (int i = 1; i <= 5; i++) {
             for (int j = 5; j > i; j--) {
-                System.out.print(" ");
+                System.out.print("  ");
             }
             for (int k = 1; k <= i; k++) {
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println("");
         }
@@ -68,10 +68,10 @@ public class Starpattern {
         System.out.println("");
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < i; j++) {
-                System.out.print(" ");
+                System.out.print("  ");
             }
             for (int k = 5; k > i; k--) {
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println("");
         }
@@ -80,20 +80,120 @@ public class Starpattern {
         System.out.println("");
         for (int i = 1; i <= 5; i++) {
             for (int j = 5; j > i; j--) {
-                System.out.print(" ");
+                System.out.print("  ");
             }
             for (int k = 1; k <= i; k++) {
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println("");
         }
         for (int i = 1; i < 5; i++) {
             for (int j = 0; j < i; j++) {
-                System.out.print(" ");
+                System.out.print("  ");
             }
             for (int k = 5; k > i; k--) {
-                System.out.print("*");
+                System.out.print("* ");
             }
+            System.out.println("");
+        }
+        System.out.println("");
+        System.out.println("9. Type ");
+        System.out.println("");
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 5; j > i; j--) {
+                System.out.print("  ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            for (int j = 1; j < i; j++) {
+                System.out.print("* ");
+            }    
+            System.out.println("");
+        }
+        System.out.println("");
+        System.out.println("10. Type ");
+        System.out.println("");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 5; k > i; k--) {
+                System.out.print("* ");
+            }
+            for (int j = 4; j > i; j--) {
+                System.out.print("* ");
+            }    
+            System.out.println("");
+        }
+        System.out.println("");
+        System.out.println("11. Type ");
+        System.out.println("");
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 5; j > i; j--) {
+                System.out.print("  ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            for (int j = 1; j < i; j++) {
+                System.out.print("* ");
+            }    
+            System.out.println("");
+        }
+        for (int i = 1; i <=5; i++) {
+            for (int j = 0; j <i; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 3; k >= i; k--) {
+                System.out.print("* ");
+            }
+            for (int j = 4; j >= i; j--) {
+                System.out.print("* ");
+            }    
+            System.out.println("");
+        }
+        System.out.println("");
+        System.out.println("12. Type ");
+        System.out.println("");
+        
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 5; k > i; k--) {
+                System.out.print("* ");
+            }
+            for (int j = 4; j > i; j--) {
+                System.out.print("* ");
+            }    
+            System.out.println("");
+        }
+        for (int i = 2; i <= 5; i++) {
+            for (int j = 5; j > i; j--) {
+                System.out.print("  ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            for (int j = 1; j < i; j++) {
+                System.out.print("* ");
+            }    
+            System.out.println("");
+        }
+        System.out.println("");
+        System.out.println("13. Type ");
+        System.out.println("");
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 5; j > i; j--) {
+                System.out.print("  ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            for (int j = 1; j < i; j++) {
+                System.out.print("* ");
+            }    
             System.out.println("");
         }
     }
