5405cd70299ae69c07755bd9e4169ff0cf1f23ec
Aryan94581 <swamiaryan611@gmail.com>
2024-07-09 22:57:22 +0530

    update

--- Starpattern.java
+++ Starpattern.java
@@ -198,6 +198,24 @@ public class Starpattern {
                 }
             }
             System.out.println();
-    }
+        }
+        System.out.println("");
+        System.out.println("13. Type ");
+        System.out.println("");
+        n = 5;
+        for (int row = 1; row <= n; row++) {
+            for (int space = 0; space < n-row; space++) {
+                System.out.print("  ");
+            }
+            for (int col = row; col >=1; col--) {
+                System.out.print(col + " ");
+            }
+            for (int col = 2; col <= row; col++) {
+                System.out.print(col + " ");
+            }
+            System.out.println("");
+        }
+
+
     }
 }
