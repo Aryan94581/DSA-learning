d8c7ed7dfafdbb6d443170476001fe776e82269c
Aryan94581 <swamiaryan611@gmail.com>
2024-07-11 19:04:52 +0530

    update

--- Starpattern.java
+++ Starpattern.java
@@ -25,7 +25,7 @@ public class Starpattern {
 
         for (int i = 5; i >0; i--) {
             for (int j = 0; j < i; j++) {
-                System.out.print("* ");
+                System.out.print(j +" ");
             }
             System.out.println("");    
         }
