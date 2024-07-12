e668175be14b2acbe13f1fee6e6a274b3b693c0b
Aryan94581 <swamiaryan611@gmail.com>
2024-07-12 23:45:48 +0530

    update

--- A_recursiveBinary.java
+++ A_recursiveBinary.java
@@ -6,20 +6,23 @@ public class A_recursiveBinary {
     public static void main(String[] args) {
         int[] array = {1,3,6,8,15,24,38,49,52,58,69,83};
         int start = 0;
-        int end = array.length;
-    System.out.println("your value is at index of :" + binaryS(array, start, end,  49));
+        int end = array.length-1;
+        int result = binaryS(array, start, end,  49);
+    System.out.println("your value is at index of :" + result);
     }
     static int binaryS(int[] arr, int start, int end, int target){
-        int mid = (start+end)/2;
-        if(target > arr[mid]){
-            start = mid+1;
-            binaryS(arr, start, end, target);
-        }else if(target < arr[mid]){
-            end = mid-1;
-            binaryS(arr, start, end, target);
-        }
-        else{
-            return mid;
+        if(start <= end){
+            int mid = (start+end)/2;
+            if(target > arr[mid]){
+                start = mid+1;
+                return binaryS(arr, start, end, target);
+            }else if(target < arr[mid]){
+                end = mid-1;
+                return binaryS(arr, start, end, target);
+            }
+            else{
+                return mid;
+            }
         }
         return -1;
     }
